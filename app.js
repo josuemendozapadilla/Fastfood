be58e1d8150a827252eaa4ebff3fb04a4c4fdabd
JuliaQuispe <quispezuritaj@gmail.com>
2018-11-27 20:46:07 -0400

    modificando el codigo

--- app.js
+++ app.js
@@ -1,49 +1,61 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var logger = require('morgan');
 
-var index = require('./routes/index');
-var service = require('./routes/api/v1.0/services');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var restaurantsRouter = require('./routes/api/restaurantes');
+var ordenesRouter = require('./routes/api/ordenes');
+var usuariosRouter = require('./routes/api/usuarios');
+var menusRouter = require('./routes/api/menus');
+
+const port = process.env.PORT || 3000;
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/api/v1.0/', service);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api/restaurantes', restaurantsRouter);
+app.use('/api/usuarios', usuariosRouter);
+app.use('/api/ordenes', ordenesRouter);
+app.use('/api/menus', menusRouter);
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
+
+  /* res.json({
+    error: err.message
+  }) */
   res.render('error');
 });
 
-module.exports = app;
-var port = 7777;
 app.listen(port, () => {
-  console.log("server running in " + port);
-});
+  console.log('corriendo en el puerto : ' + port);
+})
+module.exports = app;
--- database/connect.js
+++ database/connect.js
@@ -1,3 +1,10 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Fastfood");
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/apirestStore", {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('conexion a mongodb exitosa');
+}).catch(err => {
+    console.log('Error en la conexion', err);
+});
 module.exports = mongoose;
